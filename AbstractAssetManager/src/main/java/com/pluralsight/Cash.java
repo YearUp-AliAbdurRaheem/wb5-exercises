ad9f03dad291a27b16fde1eb45ae1b32f2b76f54
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-30 14:45:34 -0400

    Fixed Cash Class

--- AbstractAssetManager/src/main/java/com/pluralsight/Cash.java
+++ AbstractAssetManager/src/main/java/com/pluralsight/Cash.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
-
-public class Cash {
-}
+public class Cash extends Asset {
+    public Cash(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+    
+    @Override
+    public double getValue() {
+        return getOriginalCost();
+    }
+} 
